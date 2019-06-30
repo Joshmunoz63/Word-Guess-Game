a0493ed6db3fc6e6c245b88439f1655932dde06f
Joshmunoz63 <51467071+Joshmunoz63@users.noreply.github.com>
2019-06-30 18:40:41 -0500

    Add files via upload
    background done and started working on logic in js

---
+++ game.js
@@ -0,0 +1,14 @@
+var words = ["mazda", "nissan", "ford", "dodge", "jeep", "porsche", "honda"];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray = [];
+for (var i = 0; i < words.length; i++) {
+    answerArray[i] = "_";
+}
+
+var remainingLetters = word.length;
+
+while (remainingLetters > 0) {
+    
+}
