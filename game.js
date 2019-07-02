7bc71aa0213a63e75a0370a1fb78138d6574b5b9
Joshmunoz63 <51467071+Joshmunoz63@users.noreply.github.com>
2019-07-02 17:53:24 -0500

    Add files via upload
    more work to lay out and finished up how to choose word logic

--- game.js
+++ game.js
@@ -1,14 +1,20 @@
+// Words to pick from
 var words = ["mazda", "nissan", "ford", "dodge", "jeep", "porsche", "honda"];
 
+// This is what selects the random word
 var word = words[Math.floor(Math.random() * words.length)];
 
-var answerArray = [];
-for (var i = 0; i < words.length; i++) {
-    answerArray[i] = "_";
+console.log(word)
+
+// creates the right amount of underscores for the word picked
+var underscoreArray = [];  {
+    for (var i = 0; i < words.length; i++) {
+        underscoreArray.push("_");
+        
+    }
 }
 
-var remainingLetters = word.length;
+document.addEventListener(onkeyup, (event))  {
+    console.log(event);
+};
 
-while (remainingLetters > 0) {
-    
-}
